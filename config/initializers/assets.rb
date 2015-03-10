c8349159c6c084a97f95bff3428901763f867257
pedrocarmona <pcarmona1990@gmail.com>
2015-03-10 06:51:58 +0100

    Assets precompile images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,3 +3,5 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 
 #Precompile additional assets
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
